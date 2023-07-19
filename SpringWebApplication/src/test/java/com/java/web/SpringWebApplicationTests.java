04a5e97297a7933b564f507ce6d710b8abedb27d
unknown <ashi.lashkary@gmail.com>
2023-07-19 18:09:54 +0530

    first Commit

---
+++ SpringWebApplication/src/test/java/com/java/web/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
